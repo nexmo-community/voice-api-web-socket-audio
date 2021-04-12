c2a5421bc1c233df935c1a5e7182bf9efd8e8bff
Abdul Ajetunmobi <abdul.ajetunmobi@vonage.com>
2021-04-12 12:57:53 +0100

    tutorial

---
+++ index.js
@@ -0,0 +1,62 @@
+'use strict'
+
+const express = require('express');
+const WaveFile = require('wavefile').WaveFile;
+const fs = require('fs');
+
+const app = express();
+const expressWs = require('express-ws')(app);
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+expressWs.getWss().on('connection', function (ws) {
+    console.log('Websocket connection is open');
+});
+
+app.get('/webhooks/answer', (req, res) => {
+    let nccoResponse = [
+        {
+            "action": "talk",
+            "text": "Please wait while we connect you to the echo server"
+        },
+        {
+            "action": "connect",
+            "from": "NexmoTest",
+            "endpoint": [
+                {
+                    "type": "websocket",
+                    "uri": `wss://${req.hostname}/socket`,
+                    "content-type": "audio/l16;rate=16000",
+                }
+            ]
+        }
+    ]
+
+    res.status(200).json(nccoResponse)
+});
+
+app.post('/webhooks/events', (req, res) => {
+    console.log(req.body)
+    res.sendStatus(200);
+});
+
+app.ws('/socket', async (ws, req) => {
+    const wav = new WaveFile(fs.readFileSync("./sound.wav"));
+    wav.toSampleRate(16000);
+    wav.toBitDepth("16");
+    const samples = chunkArray(wav.getSamples()[0], 320)
+    for (var index = 0; index < samples.length; ++index) {
+        ws.send(Uint16Array.from(samples[index]).buffer);
+    }
+});
+
+function chunkArray(array, chunkSize) {
+    var chunkedArray = [];
+    for (var i = 0; i < array.length; i += chunkSize)
+        chunkedArray.push(array.slice(i, i + chunkSize));
+    return chunkedArray;
+}
+
+app.listen(port, () => console.log(`Listening on port ${port}`))
